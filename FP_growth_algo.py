7869c2d04ec2ebe6bb612dee74248b1282450d7a
Cometrue <96596469+ComeTrue666666@users.noreply.github.com>
2023-10-25 20:03:08 -0400

    Update FP_growth_algo.py

--- FP_growth_algo.py
+++ FP_growth_algo.py
@@ -1,5 +1,3 @@
-
-
 def convert(string):
     li = list(string.split(" "))
     return li
@@ -7,12 +5,13 @@ def convert(string):
 
 class Node(object):
     data = None
-    sup_count = 0
+    sup_count = 1
 
     def __init__(self, data):
         self.data = data
         self.sup_count = 1
         self.children = []
+        self.parent = None
 
     def add_child(self, child):
         self.children.append(child)
@@ -36,7 +35,8 @@ class Node(object):
         return temp
 
     def print_children(self):
-        print("Children: ", self.children)
+        for item in self.children:
+            print("Data = %s   Count = %d" % (item.get_data(), item.get_count()))
 
     def print_count(self):
         print("Support Count: ", self.sup_count)
@@ -44,27 +44,38 @@ class Node(object):
     def print_data(self):
         print("Data: ", self.data)
 
+    def print_node(self):
+        print("Data: ", self.data, "   [Count=", self.sup_count, "]")
+
 
 def traverse(root):
     if root is None:
         return
-    print("Current Node: ", root.data)
+    print(" ")
+    print("Node: ", root.data)
     print("Count: ", root.sup_count)
-    print("Children: ")
-    for i in range(0, len(root.children)-1):
-        print(root.children[i].data, "(", root.children[i].sup_count, ")")
-    print("\n")
+    print("Children nodes: ")
+    if not root.children:
+        print("No children node")
+    else:
+        root.print_children()
+        # for i in range(0, len(root.children)-1):
+        #     print(root.children[i].get_data(), "[count=", root.children[i].get_count(), "]")
+
+    print(" ")
     for i, node in enumerate(root.children):
         traverse(node)
 
 
-def init_tree(node, transaction, index):
+def init_tree(node, transaction, index, prev):
     node.children.append(Node(transaction[index]))
+    node.parent = prev
+    temp = node
     node = node.children[0]
     if index+1 >= len(transaction):
         return
     else:
-        init_tree(node, transaction, index + 1)
+        init_tree(node, transaction, index + 1, temp)
 
 
 def update_tree(node, transaction):
@@ -72,24 +83,23 @@ def update_tree(node, transaction):
     if not transaction:
         return
     else:
-        if transaction[0] == node.data:
-            node.inc_count()
-            transaction.pop(0)
-            update_tree(node, transaction)
-            return
-
-        else:
-            for i, child in enumerate(node.children):
-                #print("Child data: ", child.data)
-                if transaction[0] == child.data:
-                    child.inc_count()
-                    transaction.pop(0)
-                    update_tree(node.children[i], transaction)
-                    return
-            node.children.append(Node(transaction[0]))
-            transaction.pop(0)
-            update_tree(node.children[len(node.children)-1], transaction)
-            return
+        # if transaction[0] == node.data:
+        #     node.inc_count()
+        #     transaction.pop(0)
+        #     #update_tree(node, transaction)
+        #     if not transaction:
+        #         return
+        for i, child in enumerate(node.children):
+            #print("Child data: ", child.data)
+            if transaction[0] == child.data:
+                node.children[i].inc_count()
+                transaction.pop(0)
+                update_tree(node.children[i], transaction)
+                return
+        node.children.append(Node(transaction[0]))
+        transaction.pop(0)
+        update_tree(node.children[len(node.children)-1], transaction)
+        return
 
 
 def partition(array, low, high, fq_pattern):
@@ -173,7 +183,6 @@ def filter_min(fq_pattern, min_sup):
 
 def construct_tree(dataset, fq_pattern, min_sup):
     tree = Node(None)
-    #tree.set_count(0)
     tree.set_count(None)
 
     for transaction in dataset:
@@ -184,25 +193,83 @@ def construct_tree(dataset, fq_pattern, min_sup):
         quickSort(temp, 0, len(temp)-1, fq_pattern)
 
         if len(tree.children) == 0:
-            init_tree(tree, temp, 0)
-           # traverse(tree)
+            init_tree(tree, temp, 0, tree)
             continue
 
         else:
             update_tree(tree, temp)
 
-    traverse(tree)
-        # else:
-        #     update_tree(tree, transaction)
+    return tree
+
+
+def condition_pattern_base(tree, fq_pattern):
+    leaves = [[Node(2) for i in range(8)] for j in range(8)]
+    node_list = list()
+    nodes = list()
+    nodes.append(tree)
+    check_parent = False
+
+    while len(nodes):
+        # print("Node[0]: ", nodes[0].print_node())
+        curr = nodes[0]
+        removed = nodes.pop(0)
+        if check_parent:
+            temp = node_list
+            check_parent = False
+            for i in range(len(node_list)-1, -1, -1):
+                if removed in node_list[i].get_children():
+                    break
+                else:
+                    temp.pop()
+                    #print("Poped from node list: ", poped.print_node())
+            #temp.append(removed)
+            # print("node list: ")
+            # for element in temp:
+            #     element.print_node()
+            # print("Current Node: ", curr.print_node())
+            node_list = temp
+            node_list.append(removed)
+            for i in range(len(curr.children) - 1, -1, -1):
+                nodes.insert(0, curr.children[i])
+            continue
+
+        if not removed.get_children():
+            node_list.append(removed)
+            # temp_list = node_list
+            # temp_list.pop(0)
+            print("Type of node list: ", type(node_list[0]))
+            print("Node list: ")
+            for element in node_list:
+                element.print_node()
+            leaves.append(node_list)
+
+            node_list.pop()
+            check_parent = True
+            continue
+        node_list.append(removed)
+        for i in range(len(curr.children)-1, -1, -1):
+            nodes.insert(0, curr.children[i])
+    # print("Leaves: ")
+    # for li in leaves:
+    #     for item in li:
+    #         item.print_node()
+
+    # for items in leaves:
+    #     for i in range(0, len(items)-1):
+    #         print()
+    #         items[i].print_node()
+    #     print()
+
 
 def main():
     min_sup = 2
-    f_name = input("Please enter a file name to scan:  ")
+    f_name = "test.data"#input("Please enter a file name to scan:  ")
     dataset = read_dataset(f_name)
     fq_pattern = get_fq_pattern(dataset)
     fq_pattern = filter_min(fq_pattern, min_sup)
     print("Frequency Pattern: ", fq_pattern)
-    construct_tree(dataset, fq_pattern, min_sup)
+    tree = construct_tree(dataset, fq_pattern, min_sup)
+    condition_pattern_base(tree, fq_pattern)
 
 
 if __name__ == "__main__":
