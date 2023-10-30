d3c82192e9faf0455235fb9120b40292c7529562
Cometrue <96596469+ComeTrue666666@users.noreply.github.com>
2023-10-29 22:03:49 -0400

    Update FP_growth_algo.py

--- FP_growth_algo.py
+++ FP_growth_algo.py
@@ -126,6 +126,33 @@ class Header:
         return temp
 
 
+class TreePath:
+    count = 0
+    path = []
+
+    def __init__(self, count):
+        self.count = count
+        self.path = []
+
+    def get_count(self):
+        temp = self.count
+        return temp
+
+    def get_path(self):
+        temp = self.path
+        return temp
+
+    def set_count(self, count):
+        self.count = count
+
+    def set_path(self, path):
+        self.path = path
+
+    def print(self):
+        print("Count: ", self.count, "  Path: ", self.path)
+        print()
+
+
 def print_tree(root):
     if root is None:
         return
@@ -148,6 +175,8 @@ def print_tree(root):
 def init_tree(node, transaction, index):
 
     #print("Parent: ", node.parent[0].get_data())
+    if not transaction:
+        return
     node.children.append(Node(transaction[index], 1))
     temp = node
     node = node.children[0]
@@ -164,12 +193,6 @@ def update_tree(node, transaction):
     if not transaction:
         return
     else:
-        # if transaction[0] == node.data:
-        #     node.inc_count()
-        #     transaction.pop(0)
-        #     #update_tree(node, transaction)
-        #     if not transaction:
-        #         return
         for i, child in enumerate(node.children):
             #print("Child data: ", child.data)
             if transaction[0] == child.data:
@@ -254,7 +277,7 @@ def get_fq_pattern(dataset):
 def filter_min(fq_pattern, min_sup):
     del_list = list()
     for key in fq_pattern:
-        if fq_pattern[key] < min_sup:
+        if int(fq_pattern[key]) < int(min_sup):
             del_list.append(key)
 
     for key in del_list:
@@ -263,7 +286,7 @@ def filter_min(fq_pattern, min_sup):
     return fq_pattern
 
 
-def construct_tree(dataset, fq_pattern, min_sup):
+def construct_tree(dataset, fq_pattern):
     tree = Node(None, 0)
     tree.set_count(None)
 
@@ -272,6 +295,7 @@ def construct_tree(dataset, fq_pattern, min_sup):
         for element in transaction:
             if element in fq_pattern.keys():
                 temp.append(element)
+        temp = list(set(temp))
         quickSort(temp, 0, len(temp)-1, fq_pattern)
 
         if len(tree.children) == 0:
@@ -315,6 +339,7 @@ def extract_path(header):
     li = []
 
     for pointer in header.pointerList:
+        #print("Current Header: ", header.get_pointer())
         condition_pattern = ConditionPattern(0)
         curr = pointer
         condition_pattern.set_count(curr.get_count())
@@ -327,107 +352,111 @@ def extract_path(header):
             condition_pattern.pattern.reverse()
             li.append(condition_pattern)
 
-    for element in li:
-        print("Pattern: ", element.pattern, "  Count: ", element.get_count())
-
+    # for element in li:
+    #      print("Pattern: ", element.pattern, "  Count: ", element.get_count())
     return li
 
-# def find_common_pattern(data_path):
-#     common_sublist = []
-#     for pattern in data_path:
-#
 
-
-def update_cond_fp_tree(node, pattern_list):
-    if not pattern_list.pattern:
-        return
-    else:
-        for i, child in enumerate(node.children):
-            #print("Child data: ", child.data)
-            if pattern_list.pattern[0] == child.data:
-                node.children[i].inc_count_value(pattern_list.count)
-                pattern_list.pattern.pop(0)
-                update_tree(node.children[i], pattern_list.pattern)
-                return
-        node.children.append(Node(pattern_list.pattern[0], pattern_list.count))
-        node.children[-1].parent.append(node)
-        pattern_list.pattern.pop(0)
-        update_tree(node.children[len(node.children)-1], pattern_list.pattern)
-        return
-
-
-def filter_tree(tree, sup):
-    if tree is None:
-        return
-
-    if tree.get_count() < sup:
-        del tree
-        return
-
-    for i, node in enumerate(tree.children):
-        filter_tree(node, sup)
-
-
-def condition_fp_tree(pattern_base):
-    tree = Node(None, 0)
+def cnd_fp_table(pattern_base):
+    fq_table = dict()
     for pattern_list in pattern_base:
-        update_cond_fp_tree(tree, pattern_list)
-        filter_tree(tree, 2)
-    return tree
+        for item in pattern_list.pattern:
+            if item not in fq_table.keys():
+                fq_table[item] = pattern_list.get_count()
+            else:
+                fq_table[item] += pattern_list.get_count()
+    return fq_table
 
 
-def generate_combinations(elements, key):
-    all_combinations = []
+def find_paths(root, cnd_table):
+    def dfs(node, current_path):
+        if not node:
+            return
 
-    for length in range(1, len(elements) + 1):
-        for i in range(len(elements)):
-            if i + length <= len(elements):
-                combination = elements[i:i+length] + [key]
-                all_combinations.append(combination)
+        current_path.append(node.get_data())
 
-    return all_combinations
+        if not node.children:  # Leaf node
+            current_path.append(node.get_count())
+            paths.append(list(current_path))
+        else:
+            for child in node.children:
+                dfs(child, current_path)
+
+        current_path.pop()
+
+    paths = []
+    dfs(root, [])
+    return paths
 
 
-def generate_pattern(header_table):
+def generate_list(paths):
+    output = []
+    for path in paths:
+        path_list = TreePath(0)
+        path_list.set_count(path[len(path)-1])
+        path.pop(0)
+        path.pop()
+        path_list.set_path(path)
+        output.append(path_list)
+
+    return output
+
+
+def find_combination(path_list, key, cnd_table):
+    result = []
+    for paths in path_list:
+        path = paths.path
+
+        for i in range(len(paths.path)-2, 0, -1):
+            for j in range(0, i, 1):
+                if len(path) == 1:
+                    temp = TreePath(cnd_table[path[0]])
+                else:
+                    temp = TreePath(paths.get_count())
+                length = len(paths.path)-1 - i
+                temp.path.append(path[j:j+length]+[key])
+                result.append(temp)
+    return result
+
+
+def generate_pattern(header_table, min):
     key_list = header_table.keys()
-    result = dict()
-    node_list = []
-    queue = []
+    new_dataset = []
     for key in key_list:
+       # print("Current Prefix: ", key)
         pointers = header_table[key]
         data_path = extract_path(pointers)
-        tree = condition_fp_tree(data_path)
-        #print_tree(tree)
-        node_list.append(tree)
-        while node_list:
-            curr = node_list[0]
-            removed = node_list.pop(0)
-            queue.append(removed.get_data())
-           # print("Removed: ", removed.get_data())
-        # print("Queue: ", queue)
-        all_combination = generate_combinations(queue, key)
-        result[key] = all_combination
-
-    return data_path
+        cnd_table = cnd_fp_table(data_path)
+        cnd_table = filter_min(cnd_table, min)
+        for li in data_path:
+            pattern_base = li.pattern
+            new_dataset.append(pattern_base)
+        tree = construct_tree(new_dataset, cnd_table)
+        paths = find_paths(tree, cnd_table)
+    #    print("Paths: ", paths)
+        fq_pattern_list = generate_list(paths)
+        result = find_combination(fq_pattern_list, key, cnd_table)
+        for element in result:
+            print("Frequent Pattern: ", element.path, "     Count: ", element.get_count())
 
 
 def main():
     header_table = dict()
-    min_sup = 2
-    f_name = "agaricus-lepiota.data"  # input("Please enter a file name to scan:  ")
+    f_name = input("Please enter a file name to scan:  ")
+    min_sup = input("Please enter the minimum support: ")
     output_f = "output.txt"
     output_file = open("output.txt", 'w')
     dataset = read_dataset(f_name)
     fq_pattern = get_fq_pattern(dataset)
     fq_pattern = filter_min(fq_pattern, min_sup)
     print("Frequency Pattern: ", fq_pattern)
-    tree = construct_tree(dataset, fq_pattern, min_sup)
+    tree = construct_tree(dataset, fq_pattern)
     header_table = getHeader(tree, fq_pattern)
-    result = generate_pattern(header_table)
-    # for i, element in enumerate(result):
+    result = generate_pattern(header_table, min_sup)
+    #for i, element in enumerate(result):
     #     print("Path: ", element.pattern, "  Count: ", element.get_count())
     #     output_file.writelines(element.pattern)
-    # output_file.close()
+    #output_file.close()
 
 
 if __name__ == "__main__":
